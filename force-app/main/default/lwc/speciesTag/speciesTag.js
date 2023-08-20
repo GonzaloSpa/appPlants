49fb54b8eda9eb4f9c91879be49ef76a62073a3e
gonzalodp <gonzalospaltro98@gmail.com>
2023-08-20 16:19:28 -0300

    creando las carts

---
+++ force-app/main/default/lwc/speciesTag/speciesTag.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class SpeciesTag extends LightningElement {
+    specie = {
+        Name: "Jazmin",
+        Description: "Olorosa y bonita planta trepadora",
+        Image_URL__c:
+            "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcT1HpopvAg4M0gTmLR2-36Ek8jxLZPE6iXtdvFZUuNR1ERfg0a_G8ZHWCvamADnwNWZ",
+        Location__c: "Indoors,Outdoors"
+    }; 
+}
